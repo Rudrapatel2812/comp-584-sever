1b5b9f2d05111b90c810f186059f798d17f7b123
Rudrapatel2812 <patelrudra854@gmail.com>
2025-09-26 18:09:18 -0700

    forth lec

---
+++ WorldModel/City.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace WorldModel;
+
+[Table("City")]
+public partial class City
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("CountryID")]
+    public int CountryId { get; set; }
+
+    [Column("name")]
+    [StringLength(10)]
+    public string Name { get; set; } = null!;
+
+    [Column("lat")]
+    public int Lat { get; set; }
+
+    [Column("lng")]
+    public int Lng { get; set; }
+
+    [Column("population")]
+    public int Population { get; set; }
+
+    [ForeignKey("CountryId")]
+    [InverseProperty("Cities")]
+    public virtual Country Country { get; set; } = null!;
+}
---
+++ WorldModel/Migrations/20250927005455_initial.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorldModel.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Country",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    iso2 = table.Column<string>(type: "char(2)", unicode: false, fixedLength: true, maxLength: 2, nullable: false),
+                    iso3 = table.Column<string>(type: "char(3)", unicode: false, fixedLength: true, maxLength: 3, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Country", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "City",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CountryID = table.Column<int>(type: "int", nullable: false),
+                    name = table.Column<string>(type: "nchar(10)", fixedLength: true, maxLength: 10, nullable: false),
+                    lat = table.Column<int>(type: "int", nullable: false),
+                    lng = table.Column<int>(type: "int", nullable: false),
+                    population = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_City", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_City_Country",
+                        column: x => x.CountryID,
+                        principalTable: "Country",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_City_CountryID",
+                table: "City",
+                column: "CountryID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "City");
+
+            migrationBuilder.DropTable(
+                name: "Country");
+        }
+    }
+}
