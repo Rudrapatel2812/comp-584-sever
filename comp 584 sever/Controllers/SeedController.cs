222179df908d09dcf3386019405c50a4396841c4
Rudrapatel2812 <patelrudra854@gmail.com>
2025-10-03 17:52:45 -0700

    5th lec

---
+++ comp 584 sever/Controllers/SeedController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WorldModel;
+
+namespace comp_584_sever.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeedController(Comp584DataContext context) : ControllerBase
+    {
+        [HttpPost("Counties")]
+        public async Task<ActionResult> PostCountry()
+        {
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpPost("Cities")]
+        public async Task<ActionResult> PostCities()
+        {
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
